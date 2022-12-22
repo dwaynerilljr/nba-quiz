1daedc5fdfb9396f2a088445d5f3b4022dd6ff8e
Dwayne Rill Jr <dwayne@dwaynerilljr.dev>
2022-12-22 10:18:56 -0500

    Finsihed Project Updated HTML to show score; Added DOM manipulation to show score in HTML element; Added scrolling back to top to view score; Animated score increase;

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const form = document.querySelector('.quiz-form');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
+    const result = document.querySelector('.result');
 
     let score = 0;
     const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
@@ -15,6 +16,21 @@ form.addEventListener('submit', e => {
         }
     })
 
-    console.log(score);
+    //show results on page
+    scrollTo(0, 0);
+    result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if (output === score) {
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 10);
 
 })
+
+//window object
+
